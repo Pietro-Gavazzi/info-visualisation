8f1f8fb478486a216fb115cb95ae8d22de49e755
VVauchel <valentin.vauchel@student.uclouvain.be>
2024-12-12 21:13:36 +0100

    filter the data and sort them

---
+++ FilterData.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+from dash import Dash, html, dcc
+import plotly.express as px
+import plotly.graph_objects as go
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+from scipy.cluster.hierarchy import dendrogram, linkage
+from sklearn.cluster import DBSCAN
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.decomposition import PCA
+import umap
+
+# Load data
+buffalo_s = pd.read_csv("celeba_buffalo_s.csv")
+buffalo_l = pd.read_csv("celeba_buffalo_l.csv")
+
+# Find common image names
+common_image_names = set(buffalo_s['image_name']).intersection(set(buffalo_l['image_name']))
+
+# Filter rows where image_name is in the common set
+buffalo_s_filtered = buffalo_s[buffalo_s['image_name'].isin(common_image_names)]
+buffalo_l_filtered = buffalo_l[buffalo_l['image_name'].isin(common_image_names)]
+
+# order the data by image_name
+buffalo_s_filtered = buffalo_s_filtered.sort_values(by='image_name')
+buffalo_l_filtered = buffalo_l_filtered.sort_values(by='image_name')
+
+#save the filtered data
+buffalo_s_filtered.to_csv("celeba_buffalo_s_reworked.csv", index=False)
+buffalo_l_filtered.to_csv("celeba_buffalo_l_reworked.csv", index=False)
+
+
+# Verify
+print("Number of common rows:", len(common_image_names))
+print ("Number of rows in buffalo_s_filtered:", len(buffalo_s_filtered))
+print ("Number of rows in buffalo_l_filtered:", len(buffalo_l_filtered))
